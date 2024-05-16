cab8a5dca71e735fb3d5346ff042f1a849afb058
aalibyrm <165850693+aalibyrm@users.noreply.github.com>
2024-05-16 18:30:37 +0300

    Update login.php

--- proje/login.php
+++ proje/login.php
@@ -2,6 +2,7 @@
 if($_POST["email"] == "B231210068@sakarya.edu.tr" && $_POST["password"] == "B231210068") {
     echo "Hoşgeldin " . $_POST["email"];
     echo "<br>Girişin Onaylandı.";
+    header("refresh:2; index.html");
 } else {
     echo "Kullanıcı epostası yada şifre hatalı";
     header("refresh:2; login.html");
